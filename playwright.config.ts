0026bdaf31e3557e4ea8701d1b4b2596665e3868
Maryna Konovalova <maryna.konovalova.2022@gmail.com>
2026-08-21 14:57:42 +0200

    fix1

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://practicesoftwaretesting.com',
+
+    /* Attribute used by page.getByTestId(). See https://playwright.dev/docs/api/class-testoptions#test-options-test-id-attribute */
+    testIdAttribute: 'data-test',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -15,13 +15,14 @@ Verify username "Jane Doe" appears in the navigation bar.
 
 
 import { test, expect } from '@playwright/test';
+import { validUser } from './test-data/users';
 
 test('Verify login with valid credentials', async ({ page }) => {
-    await page.goto('https://practicesoftwaretesting.com/auth/login');
-    await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
-    await page.locator('[data-test="password"]').fill('welcome01');
-    await page.locator('[data-test="login-submit"]').click();
+    await page.goto('/auth/login');
+    await page.getByTestId('email').fill(validUser.email);
+    await page.getByTestId('password').fill(validUser.password);
+    await page.getByTestId('login-submit').click();
     await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
     await expect(page.getByRole('heading', { name: 'My account' })).toBeVisible();
-    await expect(page.locator('[data-test="nav-menu"]')).toContainText('Jane Doe');
+    await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe');
 });
