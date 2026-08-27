1893520de7d053dc0563f6e4ba6f145590dc06d9
Maryna Konovalova <maryna.konovalova.2022@gmail.com>
2026-08-27 23:34:40 +0200

    Verify-user-can-perform-sorting-by-name

--- tests/pages/HomePage.ts
+++ tests/pages/HomePage.ts
@@ -1,13 +1,17 @@
-import { Page } from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
 import { HeaderFragment } from './HeaderFragment';
 
 export class HomePage {
   readonly page: Page;
   readonly header: HeaderFragment;
+  readonly sortDropdown: Locator;
+  readonly productNames: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.header = new HeaderFragment(page);
+    this.sortDropdown = page.getByTestId('sort');
+    this.productNames = page.getByTestId('product-name');
   }
 
   async goto() {
@@ -16,5 +20,9 @@ export class HomePage {
 
   async openProduct(name: string) {
     await this.page.getByTestId("product-name").filter({ hasText: name }).click();
+  }
+
+  async sortBy(value: string) {
+    await this.sortDropdown.selectOption(value);
   }
 }
